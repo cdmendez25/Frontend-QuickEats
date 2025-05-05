32f283008e6878ab4c6f98eec51940cd6cc765a0
Carlos Andres Diaz Mendez <cdiazmendez25@gmail.com>
2025-05-05 08:55:57 -0500

    Ya

--- src/pages/LoginView.jsx
+++ src/pages/LoginView.jsx
@@ -20,7 +20,7 @@ export default function LoginView() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:3001/auth/login", form);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, form);
       const { token } = res.data;
       
       // Decodificar el token para obtener el rol (puedes usar jwt-decode o simplemente almacenar el rol)
